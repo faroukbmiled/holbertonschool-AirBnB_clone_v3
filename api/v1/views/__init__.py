6e98ed7c5bca86645e1375d1964e69c728f1759b
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-19 23:42:28 +0100

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,3 @@
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views import states
 from api.v1.views import cities
@@ -7,3 +5,5 @@ from api.v1.views import amenities
 from api.v1.views import places
 from api.v1.views import users
 from api.v1.views import places_reviews
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
